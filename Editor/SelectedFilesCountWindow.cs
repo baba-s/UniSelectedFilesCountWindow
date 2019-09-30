5d5a475bcd2dc8d80933813f58e87dd26f4aba0d
baba_s <shotababa1206@gmail.com>
2019-09-30 15:59:33 +0900

    first commit

---
+++ Editor/SelectedFilesCountWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace KoganeUnityLib
+{
+	internal sealed class SelectedFilesCountWindow : EditorWindow
+	{
+		private int m_count;
+
+		[MenuItem( "Window/Selected Files Count" )]
+		private static void Init()
+		{
+			var win = GetWindow<SelectedFilesCountWindow>( "Selected Files" );
+			win.minSize = new Vector2( 32, 16 );
+		}
+
+		private void OnEnable()
+		{
+			Selection.selectionChanged += OnChanged;
+		}
+
+		private void OnDisable()
+		{
+			Selection.selectionChanged -= OnChanged;
+		}
+
+		private void OnChanged()
+		{
+			m_count = Selection.objects.Length;
+			Repaint();
+		}
+
+		private void OnGUI()
+		{
+			GUILayout.Label( $"Selected Files: {m_count.ToString()}" );
+		}
+	}
+}
